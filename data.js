dae27aca1cb7a64ba654c3b431c6c4241a5a133a
Rob Elliott <elliott@iupui.edu>
2023-11-05 14:02:29 -0500

    Added data

---
+++ data.js
@@ -0,0 +1,22 @@
+const DATA = [
+    { character: "Princess Leia", 
+      quotes: [
+        "Help me Obi-Wan Kenobi, you're my only hope.",
+        "Aren't you a little short for a stormtrooper?",
+        "Why, you stuck up, half-witted, scruffy-looking Nerf herder.",
+        "I'd just as soon kiss a Wookie.",
+        "Governor Tarkin, I should have expected to find you holding Vader’s leash. I recognized your foul stench when I was brought on board.",
+        "Will someone get this big walking carpet out of my way?"
+      ]
+    },
+    { character: "Obi Wan Kenobi", 
+      quotes: [
+        "In my experience, there is no such thing as luck.",
+        "The truth is often what we make of it.",
+        "Only a sith deals in absolutes.",
+        "Who is more foolish? The fool or the fool who follows him?",
+        "I have the high ground.",
+        "If you strike me down, I shall become more powerful than you can possibly imagine."
+      ]
+    }
+]
